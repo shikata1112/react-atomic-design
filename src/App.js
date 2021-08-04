920811910f8ac37e9ed5848ac9cc12840c916c38
四方 秀 <shu.42111227@gmail.com>
2021-08-05 00:15:56 +0900

    organismsを作成

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "しゅう",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345@gmail.com",
+  phone: "090-9090-9809",
+  company: {
+    name: "aaa株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
