1c762935ae72579fe0cf8af7c66bc13117c09cb5
四方 秀 <shu.42111227@gmail.com>
2021-08-05 11:44:27 +0900

    pageを作成

---
+++ src/components/pages/Top.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Top = () => {
+  return (
+    <SContainer>
+      <h2>Topぺーじです</h2>
+    </SContainer>
+  );
+};
+
+const SContainer = styled.div`
+  text-align: center;
+`;
