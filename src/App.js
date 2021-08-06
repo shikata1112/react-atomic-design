aa4733925b7549862712d3a22aaa5525e1b30d77
四方 秀 <shu.42111227@gmail.com>
2021-08-06 16:56:58 +0900

    providerを実装 グローバルに管理できるproviderを作成

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
+import { UserProvider } from "./providers/UserProvider";
 import { Router } from "./router/Router";
 import "./styles.css";
 
-const user = {
-  name: "しゅう",
-  image: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "12345@gmail.com",
-  phone: "090-9090-9809",
-  company: {
-    name: "aaa株式会社"
-  },
-  website: "https://google.com"
-};
-
 export default function App() {
-  return <Router />;
+  return (
+    <UserProvider>
+      <Router />
+    </UserProvider>
+  );
 }
