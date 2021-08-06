3fdbf2c050cf421486f7129bb82b4e437ae56288
四方 秀 <shu.42111227@gmail.com>
2021-08-06 17:13:50 +0900

    providerをadminの判定に適応

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,13 +1,22 @@
+import { useContext } from "react";
 import { useHistory } from "react-router-dom";
 import styled from "styled-components";
+import { UserContext } from "../../providers/UserProvider";
 import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
   const history = useHistory();
-  const onClickAdmin = () =>
-    history.push({ pathname: "/users", state: { isAdmin: true } });
-  const onClickGeneral = () =>
-    history.push({ pathname: "/users", state: { isAdmin: false } });
+  const { setUserInfo } = useContext(UserContext);
+
+  const onClickAdmin = () => {
+    setUserInfo({ isAdmin: true });
+    history.push("/users");
+  };
+
+  const onClickGeneral = () => {
+    setUserInfo({ isAdmin: false });
+    history.push("/users");
+  };
 
   return (
     <SContainer>
